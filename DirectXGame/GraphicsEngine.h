d9b2efdc31dea33fdd0429d413431f8b2feaec33
Azzle <eliott.chauchard@orange.fr>
2023-12-28 16:36:09 +0100

    Basic shader + display triangle

--- DirectXGame/GraphicsEngine.h
+++ DirectXGame/GraphicsEngine.h
@@ -3,6 +3,7 @@
 
 class DeviceContext;
 class SwapChain;
+class VertexBuffer;
 
 
 class GraphicsEngine
@@ -18,6 +19,12 @@ public:
 public:
 	SwapChain* createSwapChain();
 	DeviceContext* getImmediateDeviceContext();
+	VertexBuffer* createVertexBuffer();
+
+public:
+	bool createShaders();
+	bool setShaders();
+	void getShaderBufferAndSize(void** bytecode, UINT* size);
 
 public:
 	static GraphicsEngine* get();
@@ -32,8 +39,16 @@ private:
 	IDXGIDevice* m_dxgi_device;
 	IDXGIAdapter* m_dxgi_adapter;
 	IDXGIFactory* m_dxgi_factory;
+	ID3D11DeviceContext* m_imm_context;
+
+private:
+	ID3DBlob* m_vsblob = nullptr;
+	ID3DBlob* m_psblob = nullptr;
+	ID3D11VertexShader* m_vs = nullptr;
+	ID3D11PixelShader* m_ps = nullptr;
 
 private:
 	friend class SwapChain;
+	friend class VertexBuffer;
 };
 
